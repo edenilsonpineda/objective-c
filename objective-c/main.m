1776e1c62e234907b345f3c3be1ba1cd0b91f8c7
Edenilson Pineda <edenilsonpineda@outlook.com>
2022-02-05 21:58:22 -0600

    Initial Commit

---
+++ objective-c/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  objective-c
+//
+//  Created by Edenilson Pineda on 5/2/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
